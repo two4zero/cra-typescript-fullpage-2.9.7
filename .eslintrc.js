ee7091683ab39d5c8d723745e03a19191a1a2c73
two4zero <two4zero@naver.com>
2023-04-21 11:59:46 +0900

    feat: lint 재설정

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  /*
+  env: {
+    browser: true,
+    es2021: true,
+  },
+   */
+  extends: ["react-app", "react-app/jest", "prettier"],
+  /*
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {},
+   */
+};
